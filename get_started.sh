b42c14fcf07a5d2ae16807f48a48706b5e9aa6dc
billpereira <william.pereira@me.com>
2022-09-23 10:37:39 +0200

    setup env

--- get_started.sh
+++ get_started.sh
@@ -9,19 +9,15 @@ pip --version
 
 sudo apt-get install ansible -y
 
-ansible --version
-
-
 echo 'all:' >> inventory.yaml
 echo '  hosts:' >> inventory.yaml
 echo '    zxplore:' >> inventory.yaml
 echo '      ansible_user: '$zxplore_user >> inventory.yaml
 echo '      ansible_password: '$password >> inventory.yaml
-echo '      # ansible_port: 10443 '>> inventory.yaml
 echo '      ansible_host: 204.90.115.200' >> inventory.yaml
-echo '      #  vars:' >> inventory.yaml
 echo '      PYZ: "/usr/lpp/IBM/cyp/v3r9/pyz"' >> inventory.yaml
 echo '      ZOAU: "/usr/lpp/IBM/zoautil"' >> inventory.yaml
+echo '      ansible_ssh_common_args: "-o StrictHostKeyChecking=no"' >> inventory.yaml
 echo '      ansible_python_interpreter: /usr/lpp/IBM/cyp/v3r9/pyz/bin/python3' >> inventory.yaml
 echo '      environment_vars:' >> inventory.yaml
 echo '        _BPXK_AUTOCVT: "ON"' >> inventory.yaml
@@ -39,5 +35,7 @@ echo '        LANG: "C"' >> inventory.yaml
 sudo apt-get install sshpass -y
 
 pip install ansible
+
+ansible --version
 
 exit
